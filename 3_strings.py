1ea8fea5fe4d9399a1d4401cf4d72b69dcb20091
Toni <tonivalverdesoto@gmail.com>
2024-11-21 19:48:12 +0100

    Tercer commit

---
+++ 3_strings.py
@@ -0,0 +1,19 @@
+### Strings###
+
+# Declaración de strings usando comillas simples y dobles
+variable1 = 'Antonio'
+variable2 = "Valverde"
+
+print(variable1)
+print(variable2)
+
+# Concatenar strings con un espacio en blanco
+print(variable1 + " " + variable2)
+
+# Crear un salto de línea
+variable3 = "Esto es un string\ncon salto de línea"
+print(variable3)
+
+# Insertar tabulación
+variable4 = "\tEsto es un string con una tabulación"
+print(variable4)
